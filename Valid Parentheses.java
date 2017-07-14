065dd74c75a8607e9c9cec84dda9555b76233de2
linnumberone <linnumberone@gmail.com>
2017-07-14 23:10:15 +0800

    Update Valid Parentheses.java

--- Valid Parentheses.java
+++ Valid Parentheses.java
@@ -1,3 +1,26 @@
+/*
+Solution 2:
+*/
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> chars = new Stack<Character>();
+        for(char c : s.toCharArray()) {
+            if(c == '(')
+                chars.push(')');
+            else if(c == '{')
+                chars.push('}');
+            else if(c == '[')
+                chars.push(']');
+            else if(chars.empty() || chars.pop() != c)
+                return false;
+        }
+        return chars.empty();
+    }
+}
+
+/*
+Solution 1:
+*/
 public class Solution {
     public boolean isValid(String s) {
         Stack<Character> chars = new Stack<Character>();
