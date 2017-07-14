83eab891f3fcdfdcff9de5a4fee454b09d7f43c1
linnumberone <linnumberone@gmail.com>
2017-07-14 09:51:53 +0800

    Update and rename RomanToInteger to RomanToInteger.java

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -1,3 +1,8 @@
+/*
+1. 必須要知道羅馬數字所代表的意思
+2. 要知道羅馬數字的解讀必須從後往前看，再配合加減前面的值
+*/
+
 public class Solution {
     public int romanToInt(String s) {
         int result = 0;
@@ -5,25 +10,25 @@ public class Solution {
             char c = s.charAt(i);
             switch(c) {
                 case 'I': // 1
-                    result += (result > 5 ? -1 : 1);
+                    result += (result >= 5 ? -1 : 1);
                     break;
                 case 'V': // 5
                     result += 5;
                     break;
                 case 'X': // 10
-                    result += 10 * (result > 50 ? -1 : 1);
+                    result += 10 * (result >= 50 ? -1 : 1);
                     break;
                 case 'L': // 50
                     result += 50;
                     break;
                 case 'C': // 100
-                    result += 100 * (result > 500 ? -1 : 1);
+                    result += 100 * (result >= 500 ? -1 : 1);
                     break;
                 case 'D': // 500
                     result += 500;
                     break;
                 case 'M': // 1000
-                    result += 1000 * (result > 5000 ? -1 : 1);
+                    result += 1000;
                     break;
             }
         }
