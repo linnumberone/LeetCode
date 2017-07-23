5a23a28e4336d325fd93943e8c1f8998fedae7ee
linnumberone <linnumberone@gmail.com>
2017-07-23 14:39:32 +0800

    Update Remove K Digits.java

--- Remove K Digits.java
+++ Remove K Digits.java
@@ -1,5 +1,42 @@
 public class Solution {
     public String removeKdigits(String num, int k) {
+        int len = num.length();
+        int i = 0;
+        
+        if(k == len) return "0";
+        
+        Stack<Character> stack = new Stack<Character>(); 
+        while(i < len) {
+            while(k > 0 && !stack.empty() && stack.peek() > num.charAt(i)) {
+                stack.pop();
+                k--;
+            }
+            
+            stack.push(num.charAt(i));
+            i++;
+        }
+        
+        while(k > 0) {
+            stack.pop();
+            k--;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(!stack.empty()) {
+            sb.append(stack.pop());
+        }
+        sb.reverse();
+        
+        while(sb.length()>1 && sb.charAt(0)=='0')
+            sb.deleteCharAt(0);
+        return sb.toString();
+    }
+}
+
+
+public class Solution {
+    public String removeKdigits(String num, int k) {
         while(k > 0) {
             int i = 0;
             int len = num.length();
