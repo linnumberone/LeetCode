892c702f59915f8935076f761a2b35b137171aab
linnumberone <linnumberone@gmail.com>
2017-08-10 10:45:27 +0800

    Create Valid Perfect Square.java

---
+++ Valid Perfect Square.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        int left = 1, right = num;
+        while(left <= right) {
+            long mid = (left + right) >>> 1;
+            if(mid * mid == num)
+                return true;
+            else if(mid * mid > num)
+                right = (int) mid - 1;
+            else
+                left = (int) mid + 1;
+        }
+        return false;
+    }
+}
